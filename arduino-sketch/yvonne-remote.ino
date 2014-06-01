57b76c80c942fd01bede1795a5b545e1623ee338
Jérôme Blanchi <d.j.a.y@free.fr>
2014-06-01 14:34:44 +0200

    Arduino - update the circuit + ready signal
    - Add leds to improve visual output and change pins
    - Add ReadyToLoop() who blink on start

--- arduino-sketch/yvonne-remote.ino
+++ arduino-sketch/yvonne-remote.ino
@@ -1,62 +1,45 @@
-/*****************************************************
-* This file is part of yvonne-remote.
-* (Arduino part)
-*
-* 2014, Jérôme Blanchi aka d.j.a.y
-* http://github.com/d-j-a-y/yvonne-remote
-*
-*    yvonne-remote is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-* 
-* descriptif....TODO
-* 
-* 
-* The circuit:
-* * LED attached from pin 13 to ground 
-* * start/stop switch attached to pin 2 from +5V
-* * 10K push down resistor attached to pin 2 from ground
-* * video pushbutton attached to pin 31 from +5V
-* * 10K push down resistor attached to pin 31 from ground
-* * TODO
-* 
-* * Note: on most Arduinos there is already an LED on the board
-* attached to pin 13.
-* 
-* created 2014
-* by Jérôme Blanchi aka d.j.a.y  <http://github.com/d-j-a-y/yvonne-remote>
-* 
-* based on Tutorial/Button example
-* by DojoDave <http://www.0j0.org>
-* modified 30 Aug 2011
-* by Tom Igoe
-*
-* based on Tutorial/Debounce example
-* created 21 November 2006
-* by David A. Mellis
-* modified 30 Aug 2011
-* by Limor Fried
-* modified 28 Dec 2012
-* by Mike Walters 
-*
-*****************************************************/
+/*
+ yvonne-remote Arduino 
+ 
+ descriptif....TODO
+ 
+ 
+ The circuit:
+ * video LED attached from pin 6 to 220ohm to ground 
+ * start state LED attached from pin 8 to 220ohm to ground 
+ * start/stop switch attached to pin 2 from +5V
+ * 120ohm push down resistor attached to pin 2 from ground
+ * video pushbutton attached to pin 4 from +5V
+ * 120ohm push down resistor attached to pin 31 from ground
+ * TODO
+ 
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+ 
+ created 2014
+ by Jérôme Blanchi aka d.j.a.y  <http://github.com/d-j-a-y/yvonne-remote>
+ 
+ based on Tutorial/Button example
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
 
+ based on TODO Tutorial/Debounce example
+ by DojoDave <http://www.0j0.org> TODO
+ modified 30 Aug 2011
+ by Tom Igoe
+
+ 
+ */
 
 // constants won't change. They're used here to 
 // set pin numbers:
-const int videoPushButtonPin = 31;     // the number of the video push button pin
+const int videoPushButtonPin = 4;     // the number of the video push button pin
 const int stopButtonPin = 2;     // the number of the start/stop button pin
-const int ledPin =  13;      // the number of the LED pin
+
+const int videoLedPin =  6;      // the number of the video LED pin
+const int startLedPin = 8;     // the number of the start state LED pin
+
 
 // video variables will change:
 int videoPushButtonState = 0;     // variable for reading the pushbutton status
@@ -72,7 +55,8 @@ long debounceDelay = 50;
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
+  pinMode(videoLedPin, OUTPUT);
+  pinMode(startLedPin, OUTPUT); 
   
   // initialize the video pushbutton pin as an input:
   pinMode(videoPushButtonPin, INPUT);
@@ -84,6 +68,15 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
+
+  // Signal we are quite ready to go!
+  ReadyToLoop();  
+  
+  // init stop button and led state
+  stopButtonState = digitalRead(stopButtonPin);
+  stopPreviousState = stopButtonState;
+  
+  digitalWrite(startLedPin, stopButtonState);
 }
 
 void loop(){
@@ -105,12 +98,12 @@ void loop(){
       // if it is, the buttonState is HIGH:
       if (videoPushButtonState == HIGH) {     
         // turn LED on:    
-        digitalWrite(ledPin, HIGH);
+        digitalWrite(videoLedPin, HIGH);
         Serial.print("VIDEO");
       } 
       else {
         // turn LED off:
-        digitalWrite(ledPin, LOW);
+        digitalWrite(videoLedPin, LOW);
       }
     }
   }
@@ -128,13 +121,32 @@ void loop(){
       stopButtonState = stopReading;
       // check if the pushbutton is pressed.
       // if it is, the buttonState is HIGH:
-      if (stopButtonState == HIGH) {     
+      if (stopButtonState == HIGH) {
         Serial.print("START");
+        digitalWrite(startLedPin, HIGH);   
       } 
       else {
-        Serial.print("STOP");    
+        Serial.print("STOP");
+        digitalWrite(startLedPin, LOW);   
+        
       }
     }
   }
   stopPreviousState = stopReading;  
+}
+
+// Visual confirmation of the start
+void ReadyToLoop()
+{
+  int nbloop = 0;
+  
+  for (;nbloop < 3 ; nbloop++)
+  {
+    digitalWrite(startLedPin, HIGH);
+    digitalWrite(videoLedPin, HIGH);
+    delay(50);    
+    digitalWrite(startLedPin, LOW);
+    digitalWrite(videoLedPin, LOW);
+    delay(50); 
+  }    
 }
