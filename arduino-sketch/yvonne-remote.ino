1ad79837397dc740cd8675cba18ce93710c295a8
Jérôme Blanchi <d.j.a.y@free.fr>
2014-06-01 14:56:00 +0200

    Arduino - Add "Quit" feature
    - Add quit pushbutton who send "QUIT" msg over serial

--- arduino-sketch/yvonne-remote.ino
+++ arduino-sketch/yvonne-remote.ino
@@ -1,20 +1,23 @@
 /*
  yvonne-remote Arduino 
  
- descriptif....TODO
- 
+ Few buttons to control over serial communication few aspects of the yvonnne shooting :
+ - start/stop the shoot
+ - signal to generate a video
+ - quit the shooting
+  
  
  The circuit:
+ --> Visual Output
  * video LED attached from pin 6 to 220ohm to ground 
- * start state LED attached from pin 8 to 220ohm to ground 
+ * start state LED attached from pin 8 to 220ohm to ground
+ --> Command
+ * quit pushbutton attached to pin 14 from +5V
+ * 120ohm push down resistor attached to pin 14 from ground
  * start/stop switch attached to pin 2 from +5V
  * 120ohm push down resistor attached to pin 2 from ground
  * video pushbutton attached to pin 4 from +5V
  * 120ohm push down resistor attached to pin 31 from ground
- * TODO
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
  
  created 2014
  by Jérôme Blanchi aka d.j.a.y  <http://github.com/d-j-a-y/yvonne-remote>
@@ -34,12 +37,17 @@
 
 // constants won't change. They're used here to 
 // set pin numbers:
+const int quitPushButtonPin = 14;     // the number of the quit push button pin
 const int videoPushButtonPin = 4;     // the number of the video push button pin
 const int stopButtonPin = 2;     // the number of the start/stop button pin
 
 const int videoLedPin =  6;      // the number of the video LED pin
 const int startLedPin = 8;     // the number of the start state LED pin
 
+// quit variables will change:
+int quitPushButtonState = 0;     // variable for reading the pushbutton status
+int quitPushPreviousState = 0;     // variable for reading the pushbutton status
+long quitLastDebounceTime = 0;
 
 // video variables will change:
 int videoPushButtonState = 0;     // variable for reading the pushbutton status
@@ -57,7 +65,9 @@ void setup() {
   // initialize the LED pin as an output:
   pinMode(videoLedPin, OUTPUT);
   pinMode(startLedPin, OUTPUT); 
-  
+
+  // initialize the quit pushbutton pin as an input:
+  pinMode(quitPushButtonPin, INPUT);  
   // initialize the video pushbutton pin as an input:
   pinMode(videoPushButtonPin, INPUT);
   // initialize the stop pushbutton pin as an input:
@@ -83,6 +93,26 @@ void loop(){
   // read the state of the pushbuttons value:
   int videoReading = digitalRead(videoPushButtonPin);
   int stopReading = digitalRead(stopButtonPin);  
+  int quitReading = digitalRead(quitPushButtonPin);    
+
+  //quit Push button   
+  if(quitReading != quitPushPreviousState){
+      quitLastDebounceTime = millis();
+  }
+  
+  if ( (millis() - quitLastDebounceTime) > debounceDelay)
+  {
+    if (quitReading != quitPushButtonState)
+    {
+      quitPushButtonState = quitReading;
+      // check if the pushbutton is pressed.
+      // if it is, the buttonState is HIGH:
+      if (quitPushButtonState == HIGH) {     
+        Serial.print("QUIT");
+      } 
+    }
+  }
+  quitPushPreviousState = quitReading;
 
   //Video Push button   
   if(videoReading != videoPushPreviousState){
@@ -138,9 +168,9 @@ void loop(){
 // Visual confirmation of the start
 void ReadyToLoop()
 {
-  int nbloop = 0;
+  short nbloop = 0;
   
-  for (;nbloop < 3 ; nbloop++)
+  for (;nbloop < 4 ; nbloop++)
   {
     digitalWrite(startLedPin, HIGH);
     digitalWrite(videoLedPin, HIGH);
