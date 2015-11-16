97a2cf9789d9f6552b8ad11e6d34d59e1776dcd6
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2015-11-16 16:47:26 +0100

    Scripts - image2veejay : resize picture + trailling '/' + file to go
    * use of mogrify (imagemagick) to resize the picture before doing stuff
    * aceppt '/' at end of video/folder name
    * print file to go cmpt

--- scripts/utils/image2veejay.sh
+++ scripts/utils/image2veejay.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-version=150521
+version=151116
 extension=JPG
 resolution=800x600
 staticffmpeginstall=/home/path/to/ffmpeg-2.5.3-64bit-static
+
+#TODO for nbrepeat has new option and default
+#TODO force always landscape format
+#TODO ffmpeg static dir has parameter option / avconv . or a way to automatically detect
+#TODO third parameter (rez) check validity
 
 case $# in
   0) echo "---------------------------------------"
@@ -38,35 +43,52 @@ echo "----- -----  --------------- ----- -------- "
 #done
 #endremix
 
+#remove trailling / from directory name
+target=${1%/}
+#print the number of files to go
+numfiles=(${target}/*.${extension})
+numfiles=${#numfiles[@]}
+echo -n $numfiles
+
 ##rename sequential and duplicate the file
 a=0
-for i in $1/*.$extension; do
+
+for i in ${target}/*.$extension; do
+  cp ${i} ${i}-lq.jpg
+  mogrify -resize $resolution ${i}-lq.jpg
+
+#TODO for nbrepeat has new option and default
+##  for framerepeat; do
   new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i} $1/image-${new} 
+  cp ${i}-lq.jpg ${target}/image-${new}
+  let a=a+1
+  echo -n "-"
+##done
+
+  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
+  cp ${i}-lq.jpg ${target}/image-${new}
   let a=a+1
   echo -n "-"
 
   new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i} $1/image-${new} 
+  cp ${i}-lq.jpg ${target}/image-${new}
   let a=a+1
   echo -n "-"
 
   new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i} $1/image-${new}
+  cp ${i}-lq.jpg ${target}/image-${new}
   let a=a+1
   echo -n "-"
 
   new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i} $1/image-${new} 
+  cp ${i}-lq.jpg ${target}/image-${new}
   let a=a+1
-  echo -n "-"
-  
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i} $1/image-${new}
-  let a=a+1
-  echo -n "-|"
+
+  let "numfiles -= 1"
+  echo -n "-|${numfiles}"
 done
 
+echo ""
 echo "---------------------------------------"
 echo "debut de conversionnnnnnn "
 echo "---------------- ------   ------- --------"
@@ -74,18 +96,20 @@ echo "---------------- ------   ------- --------"
 #ajouter -intra
 
 # "%sffmpeg -f image2 -start_number %d -r 25 -i \"./%s/%s-%%05d.jpg\" -q:v 1 -vcodec mjpeg -s %s ./video/%s-%d.avi", FFMPEG_STATIC_BUILD_INSTALL, startSequence, LOWQUALITY_DIRECTORY, sceneName, LOWQUALITY_RESOLUTION, sceneName, videoIndex);
-$staticffmpeginstall/ffmpeg -f image2 -r 25 -i $1/image-%04d.jpg -q:v 1 -vcodec mjpeg -s $resolution $1.avi
-#avconv -f image2 -r 25 -i $1/image-%04d.jpg -q:v 1 -vcodec mjpeg -s $resolution $1.avi
+
+#$staticffmpeginstall/ffmpeg -f image2 -r 25 -i ${target}/image-%04d.jpg -q:v 1 -vcodec mjpeg -s $resolution ${target}.avi
+avconv -f image2 -r 25 -i ${target}/image-%04d.jpg -q:v 1 -vcodec mjpeg -s $resolution ${target}.avi
+
 #ffmpeg -f image2 -i $1/image-%04d.jpg -r 24 -vcodec mjpeg -q:v 1 -b:v 12000 -s $resolution $1.avi
 
 if [ $? = 0 ]
 then
   echo "---------------------------------------------"
-  echo "La video" $1.avi "est prete a etre joyeusement remixée"
+  echo "La video" ${target}.avi "est prete a etre joyeusement remixée"
   echo "-------------  ----------------- --------  ----------"
 else
   echo "--------------------------------------"
-  echo "Problème durant la génération de $1.avi"
+  echo "Problème durant la génération de ${target}.avi"
   echo "--- -------- -------------- --------  ------ ----"
   exit 1
 fi
