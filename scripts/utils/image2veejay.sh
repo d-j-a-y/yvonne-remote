5d86bb71cfe274111f51d61242d5056ee54d7bfe
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2015-11-16 18:21:01 +0100

    Script - image2veejay : parameter handling, framerepeat
    * basic parameter handling  TODO
    * framerepeat boucle ! a candle in the light

--- scripts/utils/image2veejay.sh
+++ scripts/utils/image2veejay.sh
@@ -3,12 +3,16 @@
 version=151116
 extension=JPG
 resolution=800x600
+framerepeat=5
 staticffmpeginstall=/home/path/to/ffmpeg-2.5.3-64bit-static
 
-#TODO for nbrepeat has new option and default
-#TODO force always landscape format
+#TODO force always landscape format (seems to be actually usefull)
 #TODO ffmpeg static dir has parameter option / avconv . or a way to automatically detect
+
+#TODO DOC list dependencies
+
 #TODO third parameter (rez) check validity
+#TODO nice option parameter ! while [ $# -gt 0 ]; do blabla shift
 
 case $# in
   0) echo "---------------------------------------"
@@ -17,31 +21,27 @@ case $# in
   echo "arg1) Nom du dossier contenant les images (qui donne le nom a la vidéo)"
   echo "arg2) Extension des images (JPG par défaut)"
 #  echo "arg3) Nombre pour la numérotation (4 par défaut)"  
-  echo "arg3) Résolution de la vidéo HxL (800x600 par défaut)"  
+  echo "arg3) Résolution de la vidéo HxL (800x600 par défaut)"
+  echo "arg4) Nombre de repetition des images (5 par défaut)"
   echo "----------      -------   --  ----- -  ---------- --------"
   exit 2 ;;
-  2) extension=$2 
+  4) framerepeat=$4
+     resolution=$3
+     extension=$2 ;;
+  3) resolution=$3
+     extension=$2 ;;
+  2) extension=$2
 esac
 
 echo "---------------------------------------"
 echo "renommage en massssssssssssssssssse !"
 echo "----- -----  --------------- ----- -------- "
 
-#valeur par defaut si pas de second parametre
-#a=${2:-0}
-#echo ${a}
-
 ###rename files to sequential numbers
 #http://stackoverflow.com/questions/3211595/renaming-files-in-a-folder-to-sequential-numbers
 #by http://stackoverflow.com/users/377927/gauteh
-#remixed
-#a=0
-#for i in $1/*.$extension; do
-#  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-#  mv ${i} $1/image-${new}
-#  let a=a+1
-#done
-#endremix
+
+#debug echo $extension $resolution $framerepeat 
 
 #remove trailling / from directory name
 target=${1%/}
@@ -57,35 +57,21 @@ for i in ${target}/*.$extension; do
   cp ${i} ${i}-lq.jpg
   mogrify -resize $resolution ${i}-lq.jpg
 
-#TODO for nbrepeat has new option and default
-##  for framerepeat; do
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i}-lq.jpg ${target}/image-${new}
-  let a=a+1
-  echo -n "-"
-##done
+  indexrepeat=framerepeat
 
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i}-lq.jpg ${target}/image-${new}
-  let a=a+1
-  echo -n "-"
+  while (( $indexrepeat > 0 ))
+  do
+    new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
+    cp ${i}-lq.jpg ${target}/image-${new}
+    let "a += 1"
 
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i}-lq.jpg ${target}/image-${new}
-  let a=a+1
-  echo -n "-"
+    echo -n "-"
 
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i}-lq.jpg ${target}/image-${new}
-  let a=a+1
-  echo -n "-"
-
-  new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-  cp ${i}-lq.jpg ${target}/image-${new}
-  let a=a+1
+    let "indexrepeat -= 1"
+  done
 
   let "numfiles -= 1"
-  echo -n "-|${numfiles}"
+  echo -n "|${numfiles}"
 done
 
 echo ""
