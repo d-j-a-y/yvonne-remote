61cb28a775fe592f6a3e77c7ff370c23566a51ee
[d.j.a.y] Jerome Blanchi <d.j.a.y@free.fr>
2016-10-31 20:03:02 +0100

    script / video2veejay - fixes
    * better arg ctrl
    * fix filename with space character
    * better output filename creation
    * clean comments

--- scripts/utils/video2veejay.sh
+++ scripts/utils/video2veejay.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-version=151113
+version=161031
 resolution=800x600
-staticffmpeginstall=/home/path/to/ffmpeg-2.5.3-64bit-static
 
-case $# in
-  0) echo "---------------------------------------"
+#TODO list dependencies
+#TODO option pour pattern matching :
+###find . -name \*.mov -exec /home/frijol/Sources/#Scripts/video2veejay.sh '{}' 1920x1080 \;
+
+help_me()
+{
+  echo "---------------------------------------"
   echo "Erreur : $0: not enough arguments"
   echo "Convertie une video dans format veejay (mjpg iframe only 800x600)"
   echo "arg1) Nom de la video"
   echo "arg2) Résolution de la vidéo HxL (800x600 par défaut)"
   echo "----------      -------   --  ----- -  ---------- --------"
-  exit 2 ;;
-  2) ;;
-  *) $resolution=$2 ;;
+}
+
+
+case $# in
+  0) help_me
+    exit 2 ;;
+  1) ;;
+  2) resolution=$2 ;;
+  *) help_me
+    exit 2 ;;
 esac
+
+inputfile=$(basename "${1}")
+#echo $filename
+extension="${inputfile##*.}"
+#echo $extension
+outputfile="${inputfile%.*}.avi"
+
+if [ -f "$outputfile" ]
+then
+    echo "---------------------- - - - "
+    echo "$outputfile existe deja ----------- - - - "
+    outputfile="${inputfile}".avi
+    echo "$outputfile seras utilise------ - - - "
+    echo "---------------------- - - - "
+
+fi
 
 echo "---------------------------------------"
 echo "debut de conversionnnnnnn "
@@ -22,22 +49,17 @@ echo "---------------- ------   ------- --------"
 # -intra Use only intra frames.
 #ajouter -intra
 
-# "%sffmpeg -f image2 -start_number %d -r 25 -i \"./%s/%s-%%05d.jpg\" -q:v 1 -vcodec mjpeg -s %s ./video/%s-%d.avi", FFMPEG_STATIC_BUILD_INSTALL, startSequence, LOWQUALITY_DIRECTORY, sceneName, LOWQUALITY_RESOLUTION, sceneName, videoIndex);
-#$staticffmpeginstall/ffmpeg -f image2 -r 25 -i $1/image-%04d.jpg -q:v 1 -vcodec mjpeg -s $resolution $1.avi
-
-avconv -i $1 -q:v 1 -vcodec mjpeg -pix_fmt yuvj422p -s $resolution $1.avi
-#avconv -f image2 -r 25 -i $1/image-%04d.jpg -q:v 1 -vcodec rawvideo -pix_fmt yuv422p -acodec none -s $resolution $1.avi
-
-#ffmpeg -f image2 -i $1/image-%04d.jpg -r 24 -vcodec mjpeg -q:v 1 -b:v 12000 -s $resolution $1.avi
+#yuvj422p
+avconv -i "${1}" -r 25 -q:v 1 -vcodec mjpeg -acodec pcm_s16le -s $resolution "${outputfile}"
 
 if [ $? = 0 ]
 then
   echo "---------------------------------------------"
-  echo "La video" $1.avi "est prete a etre joyeusement remixée"
+  echo "La video" $outputfile "est prete a etre joyeusement remixée"
   echo "-------------  ----------------- --------  ----------"
 else
   echo "--------------------------------------"
-  echo "Problème durant la génération de $1.avi"
+  echo "Problème durant la génération de $outputfile"
   echo "--- -------- -------------- --------  ------ ----"
   exit 1
 fi
