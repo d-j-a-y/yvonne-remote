513cc87e9a3e05da3907d6df54d6154a7d9f6d7f
Jerome Blanchi aka d.j.a.y <d.j.a.y@free.fr>
2017-05-26 02:50:59 +0200

    * new default : 1024*576 todo : ratio option
    * temp file in a folder - progress
    todo - check folder exist

--- scripts/utils/image2veejay.sh
+++ scripts/utils/image2veejay.sh
@@ -2,7 +2,7 @@
 
 version=151116
 extension=JPG
-resolution=800x600
+resolution=1024x576
 framerepeat=5
 staticffmpeginstall=/home/path/to/ffmpeg-2.5.3-64bit-static
 
@@ -14,14 +14,27 @@ staticffmpeginstall=/home/path/to/ffmpeg-2.5.3-64bit-static
 #TODO third parameter (rez) check validity
 #TODO nice option parameter ! while [ $# -gt 0 ]; do blabla shift
 
+
+#while [ $# -gt 0 ]
+#do
+#    echo "$1"
+#    shift
+#done
+
+
+#TODO ratio option
+
+#WIP tmp folder : check exist, remove after //// 
+
+
 case $# in
   0) echo "---------------------------------------"
   echo "Erreur : $0: not enough arguments"
-  echo "Génère une vidéo (mjpg 800x600) à partir d'une série d'images"
+  echo "Génère une vidéo (mjpg 1024x576) à partir d'une série d'images"
   echo "arg1) Nom du dossier contenant les images (qui donne le nom a la vidéo)"
   echo "arg2) Extension des images (JPG par défaut)"
 #  echo "arg3) Nombre pour la numérotation (4 par défaut)"  
-  echo "arg3) Résolution de la vidéo HxL (800x600 par défaut)"
+  echo "arg3) Résolution de la vidéo HxL (1024x576 par défaut)"
   echo "arg4) Nombre de repetition des images (5 par défaut)"
   echo "----------      -------   --  ----- -  ---------- --------"
   exit 2 ;;
@@ -35,7 +48,8 @@ esac
 
 echo "---------------------------------------"
 echo "renommage en massssssssssssssssssse !"
-echo "----- -----  --------------- ----- -------- "
+echo "frame repeat : ${framerepeat}"
+
 
 ###rename files to sequential numbers
 #http://stackoverflow.com/questions/3211595/renaming-files-in-a-folder-to-sequential-numbers
@@ -46,23 +60,47 @@ echo "----- -----  --------------- ----- -------- "
 #remove trailling / from directory name
 target=${1%/}
 #print the number of files to go
-numfiles=(${target}/*.${extension})
-numfiles=${#numfiles[@]}
-echo -n $numfiles
+filesDOTextension=(${target}/*.${extension})
+numfiles=${#filesDOTextension[@]}
+echo "$numfiles to go for ${target}.avi"
+echo "----- -----  --------------- ----- -------- "
+
 
 ##rename sequential and duplicate the file
 a=0
 
-for i in ${target}/*.$extension; do
-  cp ${i} ${i}-lq.jpg
-  mogrify -resize $resolution ${i}-lq.jpg
+#echo "TEST TEST TEST TEST TEST TEST TEST TEST "
+#echo " filesDOTextension"
+#echo "TEST TEST TEST TEST TEST TEST TEST TEST "
+
+#read  -p "hello coco : .... " xx
+
+mkdir ${target}/tmp/
+
+tmpdir=${target}/tmp
+
+
+
+
+#for i in $filesDOTextension; do
+for i in ${target}/*.${extension}; do
+
+  before=${i}
+  lqfile=${before//${target}/${tmpdir}}
+  lqfile=${lqfile}-lq.jpg
+  echo ${lqfile}
+
+  cp ${i} ${lqfile}
+#  lqfile=${tmpdir}/${i}-lq.jpg
+  mogrify -resize $resolution ${lqfile}
 
   indexrepeat=framerepeat
 
   while (( $indexrepeat > 0 ))
   do
     new=$(printf "%04d.jpg" ${a}) #04 pad to length of 4
-    cp ${i}-lq.jpg ${target}/image-${new}
+    #cp ${i}-lq.jpg ${target}/image-${new}
+    ln ${lqfile} ${tmpdir}/image-${new}
     let "a += 1"
 
     echo -n "-"
@@ -84,12 +122,13 @@ echo "---------------- ------   ------- --------"
 # "%sffmpeg -f image2 -start_number %d -r 25 -i \"./%s/%s-%%05d.jpg\" -q:v 1 -vcodec mjpeg -s %s ./video/%s-%d.avi", FFMPEG_STATIC_BUILD_INSTALL, startSequence, LOWQUALITY_DIRECTORY, sceneName, LOWQUALITY_RESOLUTION, sceneName, videoIndex);
 
 #$staticffmpeginstall/ffmpeg -f image2 -r 25 -i ${target}/image-%04d.jpg -q:v 1 -vcodec mjpeg  -pix_fmt yuvj422p -s $resolution ${target}.avi
-avconv -f image2 -r 25 -i ${target}/image-%04d.jpg -q:v 1 -vcodec mjpeg  -pix_fmt yuvj422p -s $resolution ${target}.avi
+avconv -f image2 -r 25 -i ${tmpdir}/image-%04d.jpg -q:v 1 -vcodec mjpeg -aspect 16:9 -pix_fmt yuvj422p -s $resolution ${target}.avi
 
 #ffmpeg -f image2 -i $1/image-%04d.jpg -r 24 -vcodec mjpeg -q:v 1 -b:v 12000 -s $resolution $1.avi
 
 if [ $? = 0 ]
 then
+  rm -r ${tmpdir}
   echo "---------------------------------------------"
   echo "La video" ${target}.avi "est prete a etre joyeusement remixée"
   echo "-------------  ----------------- --------  ----------"
