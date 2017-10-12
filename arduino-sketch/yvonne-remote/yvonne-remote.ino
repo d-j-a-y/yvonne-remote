b7122992c85e18fa542b5e06a938433482fb4004
Jerome Blanchi aka d.j.a.y <d.j.a.y@free.fr>
2017-10-12 13:30:34 +0200

    arduino : fix sketch folder

--- arduino-sketch/yvonne-remote/yvonne-remote.ino
+++ arduino-sketch/yvonne-remote/yvonne-remote.ino
@@ -1,15 +1,15 @@
 /*
- yvonne-remote Arduino 
- 
+ yvonne-remote Arduino
+
  Few buttons to control over serial communication few aspects of the yvonnne shooting :
  - start/stop the shoot
  - signal to generate a video
  - quit the shooting
-  
- 
+
+
  The circuit:
  --> Visual Output
- * video LED attached from pin 6 to 220ohm to ground 
+ * video LED attached from pin 6 to 220ohm to ground
  * start/stop state LED attached from pin 8 to 220ohm to ground
  --> Command
  * quit pushbutton attached to pin 14 from +5V
@@ -18,10 +18,10 @@
  * 120ohm push down resistor attached to pin 2 from ground
  * video pushbutton attached to pin 4 from +5V
  * 120ohm push down resistor attached to pin 31 from ground
- 
+
  created 2014
  by Jérôme Blanchi aka d.j.a.y  <http://github.com/d-j-a-y/yvonne-remote>
- 
+
  based on Tutorial/Button example
  by DojoDave <http://www.0j0.org>
  modified 30 Aug 2011
@@ -32,10 +32,10 @@
  modified 30 Aug 2011
  by Tom Igoe
 
- 
+
  */
 
-// constants won't change. They're used here to 
+// constants won't change. They're used here to
 // set pin numbers:
 const int quitPushButtonPin = 14;     // the number of the quit push button pin
 const int videoPushButtonPin = 4;     // the number of the video push button pin
@@ -64,15 +64,15 @@ long debounceDelay = 50;
 void setup() {
   // initialize the LED pin as an output:
   pinMode(videoLedPin, OUTPUT);
-  pinMode(startLedPin, OUTPUT); 
+  pinMode(startLedPin, OUTPUT);
 
   // initialize the quit pushbutton pin as an input:
-  pinMode(quitPushButtonPin, INPUT);  
+  pinMode(quitPushButtonPin, INPUT);
   // initialize the video pushbutton pin as an input:
   pinMode(videoPushButtonPin, INPUT);
   // initialize the stop pushbutton pin as an input:
-  pinMode(stopButtonPin, INPUT);  
-  
+  pinMode(stopButtonPin, INPUT);
+
   // Open serial (default 8N1) communications and wait for port to open:
   Serial.begin(57600);
   while (!Serial) {
@@ -80,8 +80,8 @@ void setup() {
   }
 
   // Signal we are quite ready to go!
-  ReadyToLoop();  
-  
+  ReadyToLoop();
+
   // init stop button and led state
   stopButtonState = digitalRead(stopButtonPin);
   stopPreviousState = stopButtonState;
@@ -92,14 +92,14 @@ void setup() {
 void loop(){
   // read the state of the pushbuttons value:
   int videoReading = digitalRead(videoPushButtonPin);
-  int stopReading = digitalRead(stopButtonPin);  
-  int quitReading = digitalRead(quitPushButtonPin);    
+  int stopReading = digitalRead(stopButtonPin);
+  int quitReading = digitalRead(quitPushButtonPin);
 
-  //quit Push button   
+  //quit Push button
   if(quitReading != quitPushPreviousState){
       quitLastDebounceTime = millis();
   }
-  
+
   if ( (millis() - quitLastDebounceTime) > debounceDelay)
   {
     if (quitReading != quitPushButtonState)
@@ -107,18 +107,18 @@ void loop(){
       quitPushButtonState = quitReading;
       // check if the pushbutton is pressed.
       // if it is, the buttonState is HIGH:
-      if (quitPushButtonState == HIGH) {     
+      if (quitPushButtonState == HIGH) {
         Serial.print("QUIT");
-      } 
+      }
     }
   }
   quitPushPreviousState = quitReading;
 
-  //Video Push button   
+  //Video Push button
   if(videoReading != videoPushPreviousState){
       videoLastDebounceTime = millis();
   }
-  
+
   if ( (millis() - videoLastDebounceTime) > debounceDelay)
   {
     if (videoReading != videoPushButtonState)
@@ -126,11 +126,11 @@ void loop(){
       videoPushButtonState = videoReading;
       // check if the pushbutton is pressed.
       // if it is, the buttonState is HIGH:
-      if (videoPushButtonState == HIGH) {     
-        // turn LED on:    
+      if (videoPushButtonState == HIGH) {
+        // turn LED on:
         digitalWrite(videoLedPin, HIGH);
         Serial.print("VIDEO");
-      } 
+      }
       else {
         // turn LED off:
         digitalWrite(videoLedPin, LOW);
@@ -138,12 +138,12 @@ void loop(){
     }
   }
   videoPushPreviousState = videoReading;
-  
-  //Start/Stop Push button   
+
+  //Start/Stop Push button
   if(stopReading != stopPreviousState){
       stopLastDebounceTime = millis();
   }
-  
+
   if ( (millis() - stopLastDebounceTime) > debounceDelay)
   {
     if (stopReading != stopButtonState)
@@ -153,30 +153,30 @@ void loop(){
       // if it is, the buttonState is HIGH:
       if (stopButtonState == HIGH) {
         Serial.print("START");
-        digitalWrite(startLedPin, HIGH);   
-      } 
+        digitalWrite(startLedPin, HIGH);
+      }
       else {
         Serial.print("STOP");
-        digitalWrite(startLedPin, LOW);   
-        
+        digitalWrite(startLedPin, LOW);
+
       }
     }
   }
-  stopPreviousState = stopReading;  
+  stopPreviousState = stopReading;
 }
 
 // Visual confirmation of the start
 void ReadyToLoop()
 {
   short nbloop = 0;
-  
+
   for (;nbloop < 4 ; nbloop++)
   {
     digitalWrite(startLedPin, HIGH);
     digitalWrite(videoLedPin, HIGH);
-    delay(50);    
+    delay(50);
     digitalWrite(startLedPin, LOW);
     digitalWrite(videoLedPin, LOW);
-    delay(50); 
-  }    
+    delay(50);
+  }
 }
